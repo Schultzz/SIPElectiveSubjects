d63766e0254d081adae3bf9ddd36ae529c6135a2
Flazhed <soren1337@gmail.com>
2015-09-03 13:24:57 +0200

    DummyClass added for structure

---
+++ SIPElectiveStudyGrpA/src/main/java/dk/cphbussines/namemeplease/DummyClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.cphbussines.namemeplease;
+
+/**
+ *
+ * @author Søren
+ */
+public class DummyClass {
+    
+}
