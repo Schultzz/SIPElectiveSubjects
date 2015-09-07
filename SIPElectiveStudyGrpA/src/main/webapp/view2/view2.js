ee36d36eb5e9406ba1a5758169257521575d93ce
ms <ms@10.50.130.28>
2015-09-07 08:16:07 +0200

    Missing comments.
    Pair (Morten & Nikolai)

--- SIPElectiveStudyGrpA/src/main/webapp/view2/view2.js
+++ SIPElectiveStudyGrpA/src/main/webapp/view2/view2.js
@@ -2,61 +2,68 @@
 
 angular.module('myApp.view2', ['ngRoute'])
 
-        .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/view2', {
-                    templateUrl: 'view2/view2.html',
-                    controller: 'View2Ctrl'
-                });
-            }])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
+        });
+    }])
 
-        .controller('View2Ctrl', ['$scope', 'subjectsFactory', function ($scope, subjectsFactory) {
-
-                //Drag and drop elemets
-                $scope.models = {
-                    selected: null,
-                    lists: {"A": [], "Poll": [],"B": []}
-                };
-
-                // Generate initial model
-                for (var i = 1; i <= 3; ++i) {
-                    //$scope.models.lists.A.push({label: "Item A" + i});
-                    //$scope.models.lists.B.push({label: "Item B" + i});
-                    $scope.models.lists.Poll.push({label: "Item B" + i});
-                }
-                
-                $scope.test = function(){
-                    console.log("A: "+$scope.models.lists.A.length);
-                    console.log("Poll: "+$scope.models.lists.Poll.length);
-                    console.log("B: "+$scope.models.lists.B.length);
-                } 
-
-//                // Model to JSON for demo purpose
-//                $scope.$watch('models', function (model) {
-//                    $scope.modelAsJson = angular.toJson(model, true);
-//                }, true);
+    .controller('View2Ctrl', ['$scope', 'subjectsFactory', function ($scope, subjectsFactory) {
 
 
-                // HTTP request to get subjects
-                
-                subjectsFactory.getData().success(function (data) {
-                    $scope.subjets = data;
-                }).error(function (data) {
-                    $scope.subjetsError = "Fejl" + data;
-                });
+        //--------- Drag and drop logic
+
+        $scope.models = {
+            selected: null,
+            lists: {"A": [], "Poll": [], "B": []}
+        };
+
+        // TODO: move this to the HTTP request, instead of "dummy" data.
+        for (var i = 1; i <= 5; ++i) {
+            //$scope.models.lists.A.push({label: "Item A" + i});
+            //$scope.models.lists.B.push({label: "Item B" + i});
+            $scope.models.lists.Poll.push({label: "Item B" + i, name: "test " + i});
+        }
 
 
-            }])
-        .factory('subjectsFactory', ['$http', function ($http)
-            {
-                var url = "/api/getAllSubjects";
-                var dataFactory = {};
-                dataFactory.getData = function ()
-                {
-                    return $http.get(url);
-                }
-                return dataFactory;
+        // Can be removed, to test if the list were filled up.
+        $scope.test = function () {
+            console.log("A: " + $scope.models.lists.A.length);
+            console.log("Poll: " + $scope.models.lists.Poll.length);
+            console.log("B: " + $scope.models.lists.B.length);
+        };
 
-            }]);
+        // Called when a subject is moved,
+        // TODO: Calculate logic here!!
+        $scope.moved = function () {
+            console.log("MOVED!");
+        };
+
+        //--------- END. Drag and drop logic
 
 
-;
+        //--------- HTTP request to get subjects
+
+        subjectsFactory.getData().success(function (data) {
+            $scope.subjets = data;
+        }).error(function (data) {
+            $scope.subjetsError = data;
+        });
+
+        //--------- END. HTTP request to get subjects
+
+    }])
+    .factory('subjectsFactory', ['$http', function ($http) {
+
+        var dataFactory = {};
+
+
+        dataFactory.getData = function () {
+            return $http.get("/api/getAllSubjects");
+        };
+        return dataFactory;
+
+    }]);
+
+
